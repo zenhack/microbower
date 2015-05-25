e4904ce18ea68ed0a5f3244d05616fadaeb91f9c
Ian Denhardt <ian@zenhack.net>
2015-05-24 21:00:59 -0400

    Initial commit
    See the README; I got tired of doing all this manually every time I
    started a new project.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='package-name',
+      version='0.1',
+      packages=[],
+
+      author='Your Name',
+      author_email='you@example.com',
+      description='Short description',
+      license='some license[1]',
+      url='https://www.example.com/your-project',
+      )
+
+# [1] There's a good list of Open Source license here:
+#
+#   http://opensource.org/licenses/alphabetical
+#
+# The license field should be the same as the abbreviation in parens
+# on that page.
